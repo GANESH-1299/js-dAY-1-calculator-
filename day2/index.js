be0a4a836a575689bd92382aa48c99ad8846b315
GANESH-1299 <85486909+GANESH-1299@users.noreply.github.com>
2021-08-25 21:25:24 +0530

    Add files via upload

---
+++ day2/index.js
@@ -0,0 +1,27 @@
+let electronics=[
+    {
+        name:"Televsion",
+        price:120000,
+        quantity:2,
+    }
+    ,
+    {
+        name:"Refrigerator",
+        price:32000,
+        quantity:2,
+    }
+    ,
+    {
+        name:"Airconditioner",
+        price:60000,
+        quantity:2,
+    }
+]
+
+
+for(let num=0;num<electrions.length;num++){
+    console.log(food[num]);
+}
+
+
+//name,price,quantity
