32c17fc6c9c64b06757dccb46c1f97ff58106bba
GANESH-1299 <85486909+GANESH-1299@users.noreply.github.com>
2021-08-25 18:10:30 +0530

    Add files via upload

---
+++ JS-ESSENTIALS-main/DAY1/index.js
@@ -0,0 +1,24 @@
+let num1=12
+let num2=09
+sumEl=document.getElementById("sum-el")
+
+function add(){
+    sum=num1+num2
+    sumEl.innerText="SUM="+sum
+
+}
+function subtract(){
+    sum=num1-num2
+    sumEl.innerText="SUM="+sum
+
+}
+function multiply(){
+    sum=num1*num2
+    sumEl.innerText="SUM="+sum
+
+}
+function divide(){
+    sum=num1/num2
+    sumEl.innerText="SUM="+sum
+
+}
